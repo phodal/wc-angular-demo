815263828fcbb0b8c7d84aae30c42ccbb5edf640
Phodal HUANG <h@phodal.com>
2018-08-08 20:21:07 +0800

    [T] update demo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  action = event => {
+    console.log(event);
+  };
   render() {
     return (
       <div className="App">
@@ -12,6 +15,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+            <interact-bar action={this.action}></interact-bar>
         </p>
       </div>
     );
